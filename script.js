fae8f6fff650de71fc6980c3e019b308c5109ac3
Muneer Girnari <muneeragirnari@gmail.com>
2023-07-27 10:22:34 +0530

    solved script.js

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 function threeSum(arr, target) {
 // write your code here
-  
+    let [i,j,k] = [0,1,2];
+    let dif = Integter.MAX_VALUE;
+    while(i<=arr.length-3){
+        while(j<=arr.length-2 && k<=arr.length-1){
+            let sum = arr[i]+arr[j]+arr[k];
+            let ans = target - sum < dif? sum : dif;
+            j++;
+            k++;
+        }
+        i++;
+    }
+    return ans;
 }
 
 module.exports = threeSum;
